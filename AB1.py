22bf37df266d81f9639fdb3140954eb452037355
TaigoI <taigo.pedrosa@gmail.com>
2022-10-10 01:51:25 -0300

    add campo

--- AB1.py
+++ AB1.py
@@ -39,7 +39,6 @@ def Cube():
             glVertex3fv(verticies[vertex])
     glEnd()
 
-
 def Line(iX, iZ, fX, fZ, color=[1,1,1], log=False):
     glBegin(GL_LINES)
     glColor(color)
@@ -105,14 +104,44 @@ def bresenhamLinePoints(iX, iZ, fX, fZ, dX, dZ):
         points.reverse()
     return points
 
+def Campo():
+    #Limite Maior
+    Line(0,0,0,105)
+    Line(0,105,68,105)
+    Line(68,105,68,0)
+    Line(68,0,0,0)
+
+    #Meio Campo
+    Line(0,52.5,68,52.5)
+    
+    #Grande Área Superior
+    Line(14,0,14,17)
+    Line(54,0,54,17)
+    Line(14,17,54,17)
+
+    #Pequena Área Superior
+    Line(25,0,25,6)
+    Line(43,0,43,6)
+    Line(25,6,43,6)
+
+    #Grande Área Inferior
+    Line(14,105,14,88)
+    Line(54,105,54,88)
+    Line(14,88,54,88)
+
+    #Pequena Área Inferior
+    Line(25,105,25,99)
+    Line(43,105,43,99)
+    Line(25,99,43,99)
+
 def main():
     pygame.init()
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-    gluPerspective(45, (display[0]/display[1]), 0.01, 50.0)
+    gluPerspective(45, (display[0]/display[1]), 0.01, 200.0)
 
-    glTranslatef(0, -2, -7)
+    glTranslatef(-35, 53, -150)
     glRotatef(90, 1, 0, 0)
 
     while True:
@@ -148,16 +177,9 @@ def main():
 
         
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        Line(0,0,2,0, [1,0,0]) #X (vermelho)
-        Line(0,0,0,2, [0,1,0]) #Z (verde)
+        glLineWidth(3)
+        Campo()
 
-        Line(0,0,0,1)
-        Line(0,1,1,1)
-        Line(1,1,1,0)
-        Line(1,0,0,0)
-
-        Line(0,0,1,1, [1,1,0])
-        Line(0,1,1,0, [1,1,0])
         pygame.display.flip()
         pygame.time.wait(10)
 
